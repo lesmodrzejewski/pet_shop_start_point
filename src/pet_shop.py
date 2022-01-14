e597a34b8bd107c8686340e88eead5f6160ba277
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-01-14 14:17:16 +0000

    two functions done

---
+++ src/pet_shop.py
@@ -0,0 +1,10 @@
+# WRITE YOUR FUNCTIONS HERE
+
+from ssl import get_protocol_name
+
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(total_cash):
+    return total_cash["admin"]["total_cash"]
